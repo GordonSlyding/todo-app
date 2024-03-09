44f7c03d6a810a062d374261e41ff82858a51a95
Gordon Cheung <Gordoncheung00@gmail.com>
2024-03-09 20:24:55 +0100

    spec changes

--- todo-app/src/app/task.service.spec.ts
+++ todo-app/src/app/task.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TaskService } from './task.spec';
+import { TaskService } from './task.service';
 
 describe('TaskServiceService', () => {
   let service: TaskService;
